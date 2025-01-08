d7fbd05de21a1c06a464027510e0f97677a62ea8
An-dr3a <criolloandrea20@hotmail.com>
2025-01-08 12:10:43 -0500

    EJEMPLO03

--- ejemplos/Ejemplos/src/ejemplo03/Ejemplo03.java
+++ ejemplos/Ejemplos/src/ejemplo03/Ejemplo03.java
@@ -2,8 +2,12 @@
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
+Generar una solucion que permita sumar dos numeros a tra vez de un metodo, los numeros deben ser gresados por teclado en el metodo main. 
+El metodo que genera la suma debe verificar que los dos sean numeros sean positivas para realizar la operacion, caso contrario la suma 
+deber ser la suma 0
  */
-package ejemplos01;
+package ejemplo03;
+import ejemplos01.*;
 import java.util.Scanner;
 
 /**
@@ -19,10 +23,10 @@ public class Ejemplo03 {
         int valor2 = 0;
         
         System.out.println("Ingrese el primer valor");
-        valor1 = entrada.nextInt(valor1);
+        valor1 = entrada.nextInt();
         
         System.out.println("Ingrese el segundo valor");
-        valor2 = entrada.nextInt(valor1);
+        valor2 = entrada.nextInt();
         // 
         int valor = obtenerSuma(valor1, valor2); // se invoca al método 
                                          // obtenerSuma
@@ -33,8 +37,13 @@ public class Ejemplo03 {
     }
     
     
-    public static int obtenerSuma(int a, int b){
-        int suma;
+    public static int obtenerSuma(int a, int b){        
+        int suma;        
+        if (a > 0 && b > 0) {
+           suma = a + b; 
+        } else {
+            suma = 0;
+        }
         suma = a + b;
         return suma;
         // return a + b;
